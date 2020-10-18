83596b0a5f3ac0548da6e55f4257bdc4e97c53d3
nadavshtein <nadavshtein10@gmail.com>
2020-10-18 15:45:28 +0300

    manager login page xml updates

--- app/src/main/java/com/example/capsulate/login/FirstPage.java
+++ app/src/main/java/com/example/capsulate/login/FirstPage.java
@@ -2,9 +2,13 @@ package com.example.capsulate.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.capsulate.R;
+import com.ornach.nobobutton.NoboButton;
 
 public class FirstPage extends AppCompatActivity {
 
@@ -12,5 +16,33 @@ public class FirstPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+        NoboButton managerButton=findViewById(R.id.managerButton);
+        NoboButton workerButton=findViewById(R.id.workerButton);
+        setManagerButtonListener(managerButton);
+        setWorkerButtonListener(workerButton);
+
+
+    }
+
+    private void setWorkerButtonListener(NoboButton workerButton) {
+
+        workerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(v.getContext() ,WorkerLogin.class));
+            }
+        });
+    }
+
+    private void setManagerButtonListener(NoboButton managerButton) {
+
+        managerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(v.getContext() ,ManagerLogin.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/capsulate/login/WorkerLogin.java
@@ -0,0 +1,16 @@
+package com.example.capsulate.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.capsulate.R;
+
+public class WorkerLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_login);
+    }
+}
